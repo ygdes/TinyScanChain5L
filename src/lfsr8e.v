c1c3d72c2f626104139abbb0f24addf9198a5238
YGDES <262647190+ygdes@users.noreply.github.com>
2026-03-27 06:52:41 +0100

    Update lfsr8e.v

--- src/lfsr8e.v
+++ src/lfsr8e.v
@@ -13,18 +13,18 @@ module LFSR8E(
 );
   // The poly XORs
   wire x1, x2, x3; 
-  (* keep *) sg13g2_xor2_1 x_a(.X(x1), .A(LFSR_STATE[0]), .B(LFSR_STATE[5]));
-  (* keep *) sg13g2_xor2_1 x_b(.X(x2), .A(LFSR_STATE[0]), .B(LFSR_STATE[3]));
-  (* keep *) sg13g2_xor2_1 x_c(.X(x3), .A(LFSR_STATE[0]), .B(LFSR_STATE[1]));
+  (* keep *) sg13cmos5l_xor2_1 x_a(.X(x1), .A(LFSR_STATE[0]), .B(LFSR_STATE[5]));
+  (* keep *) sg13cmos5l_xor2_1 x_b(.X(x2), .A(LFSR_STATE[0]), .B(LFSR_STATE[3]));
+  (* keep *) sg13cmos5l_xor2_1 x_c(.X(x3), .A(LFSR_STATE[0]), .B(LFSR_STATE[1]));
 
   wire dum1, dum2;
   // The actual shit register, that supports "reset to 00000110"
-  (* keep *) sg13g2_sdfrbpq_1 lfsr7(            .Q(LFSR_STATE[7]), .SCD(LFSR_STATE[0]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[7]));
-  (* keep *) sg13g2_sdfrbpq_1 lfsr6(            .Q(LFSR_STATE[6]), .SCD(LFSR_STATE[7]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[6]));
-  (* keep *) sg13g2_sdfrbpq_1 lfsr5(            .Q(LFSR_STATE[5]), .SCD(LFSR_STATE[6]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[5]));
-  (* keep *) sg13g2_sdfrbpq_1 lfsr4(            .Q(LFSR_STATE[4]), .SCD(x1),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[4]));
-  (* keep *) sg13g2_sdfrbpq_1 lfsr3(            .Q(LFSR_STATE[3]), .SCD(LFSR_STATE[4]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[3]));
-  (* keep *) sg13g2_sdfrbp_1  lfsr2(.Q(dum2), .Q_N(LFSR_STATE[2]), .SCD(x2),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(dum2));
-  (* keep *) sg13g2_sdfrbp_1  lfsr1(.Q(dum1), .Q_N(LFSR_STATE[1]), .SCD(LFSR_STATE[2]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(dum1));
-  (* keep *) sg13g2_sdfrbpq_1 lfsr0(            .Q(LFSR_STATE[0]), .SCD(x3),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[0]));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr7(            .Q(LFSR_STATE[7]), .SCD(LFSR_STATE[0]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[7]));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr6(            .Q(LFSR_STATE[6]), .SCD(LFSR_STATE[7]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[6]));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr5(            .Q(LFSR_STATE[5]), .SCD(LFSR_STATE[6]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[5]));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr4(            .Q(LFSR_STATE[4]), .SCD(x1),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[4]));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr3(            .Q(LFSR_STATE[3]), .SCD(LFSR_STATE[4]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[3]));
+  (* keep *) sg13cmos5l_sdfrbp_1  lfsr2(.Q(dum2), .Q_N(LFSR_STATE[2]), .SCD(x2),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(dum2));
+  (* keep *) sg13cmos5l_sdfrbp_1  lfsr1(.Q(dum1), .Q_N(LFSR_STATE[1]), .SCD(LFSR_STATE[2]), .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(dum1));
+  (* keep *) sg13cmos5l_sdfrbpq_1 lfsr0(            .Q(LFSR_STATE[0]), .SCD(x3),            .RESET_B(RESET), .CLK(CLK), .SCE(LFSR_EN), .D(LFSR_STATE[0]));
 endmodule
